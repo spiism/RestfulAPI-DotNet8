f31f80619add17c05308d96fd5769df8ae359d6f
spiism <sp.iism@gmail.com>
2024-05-22 12:01:57 +1200

    adding entity framework & sql DB

---
+++ RestfulAPI-DotNet8/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestfulAPI_DotNet8.Entities;
+
+namespace RestfulAPI_DotNet8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet <SuperHero> SuperHeroes { get; set; }
+    }
+}
