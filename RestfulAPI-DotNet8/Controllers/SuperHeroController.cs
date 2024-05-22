4f99b0e6049c740c651e979882bbd07f8573d124
spiism <sp.iism@gmail.com>
2024-05-22 21:44:54 +1200

    DB Migration & Controller Implementation:

--- RestfulAPI-DotNet8/Controllers/SuperHeroController.cs
+++ RestfulAPI-DotNet8/Controllers/SuperHeroController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestfulAPI_DotNet8.Data;
 using RestfulAPI_DotNet8.Entities;
 
 namespace RestfulAPI_DotNet8.Controllers
@@ -8,21 +10,30 @@ namespace RestfulAPI_DotNet8.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         //public async Task<IActionResult> GetAllHeroes()
         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
-            var heroes = new List<SuperHero>
-            {
-                new SuperHero
-                {
-                    Id = 1,
-                    Name = "Spiderman",
-                    FirstName = "Perter",
-                    LastName = "Parker",
-                    Place = "New York City"
-                }
-            };
+            //var heroes = new List<SuperHero>
+            //{
+            //    new SuperHero
+            //    {
+            //        Id = 1,
+            //        Name = "Spiderman",
+            //        FirstName = "Perter",
+            //        LastName = "Parker",
+            //        Place = "New York City"
+            //    }
+            //};
+
+            var heroes = await _context.SuperHeroes.ToListAsync();
             return Ok(heroes);
         }
     }
